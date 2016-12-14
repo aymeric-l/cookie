a58e1d9d1f0b35d2c1688fee0a52dfabfa9866e5
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-14 18:03:02 +0100

    Ajout multiples

--- cookie.js
+++ cookie.js
@@ -1,26 +1,39 @@
 var score = 0;
 var prix = 50;
 var compteur = 1;
-
+var prixauto = 50;
+var depense = 0;
+var total;
 function autoclick(){
-	if(score >=50){
+	if(score >=prixauto){
 		setInterval(javabouton, 2000);
+		document.getElementById("total").innerHTML=total;
+		score=score-prixauto;
+		prixauto=prixauto*2;
+		document.getElementById("clickauto").innerHTML="Click automatique,prix :"+prixauto;
 	}
 }
 
-function javabouton(){
+
+
+function javabouton(){	
 	score=score+compteur;
-	console.log(score);
+	total = score+depense;
+	console.log(depense);
+	console.log(total);
 	document.getElementById("resultat").innerHTML=score;
+	document.getElementById("total").innerHTML="Nombre total de cookies gagnés : "+total;
 }
 
 function incrementer(){
 	if(score>=prix){
 	compteur=compteur+1;
+	depense=depense+prix;
+	document.getElementById("total").innerHTML=total;
 	score=score-prix;
 	prix = prix*2;
 	console.log(compteur);
-		document.getElementById("multiplicateur").innerHTML="Multiplicateur X"+compteur+". Prix du prochain achat : "+prix;
+		document.getElementById("multiplicateur").innerHTML="Multiplicateur actuel : X"+compteur+". Prix du prochain achat : "+prix;
 		document.getElementById("resultat").innerHTML=score;
 	}
 
